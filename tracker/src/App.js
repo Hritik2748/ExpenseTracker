af426583085f3bc2e780738bcfaff8735e8c7f4c
Hritik2748 <hritikrajsingh88@gmail.com>
2025-02-23 22:57:27 +0530

    add tracker app

---
+++ tracker/src/App.js
@@ -0,0 +1,18 @@
+
+
+import Tracker from "./component/Tracker";
+import GlobalStyles from "./globalStyles";
+
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <GlobalStyles />
+      <Tracker/>
+    </div>
+  )
+}
+
+export default App;
